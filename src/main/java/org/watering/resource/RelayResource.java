8e4c1e100b267fa9fdbcad62a6ea64dc420cf10d
andreas-repo <andreas.mayer@gepardec.com>
2020-05-22 15:37:07 +0200

    git

---
+++ src/main/java/org/watering/resource/RelayResource.java
@@ -0,0 +1,22 @@
+package org.watering.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/relay")
+public interface RelayResource {
+
+    @GET
+    @Path("/start/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response start(@PathParam("id") int id);
+
+    @GET
+    @Path("/stop/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response stop(@PathParam("id") int id);
+}
