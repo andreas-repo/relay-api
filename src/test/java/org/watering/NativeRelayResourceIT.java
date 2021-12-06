a41b633e867e48343ec0bff2130408f7a4048488
Andreas Mayer <andreas.mayer@gepardec.com>
2021-12-06 21:27:35 +0100

    removed tests

--- src/test/java/org/watering/NativeRelayResourceIT.java
+++
@@ -1,9 +0,0 @@
-package org.watering;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeRelayResourceIT extends RelayResourceTest {
-
-    // Execute the same tests but in native mode.
-}
